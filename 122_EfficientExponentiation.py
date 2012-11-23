f7f0c2928464702bac466fb3b47cb5f4ca9153d8
Ehsan Kia <contact@ehsankia.com>
2012-11-23 03:23:27 -0500

    Optimized solution to problem 122, ~3x faster.

--- 122_EfficientExponentiation.py
+++ 122_EfficientExponentiation.py
@@ -1,30 +1,34 @@
 from collections import deque
 
 # Basic BFS with a queue
-def effExp(exp):
-    if exp == 1:
-        return 0
-    expQueue = deque([(1,)]) # Starts with exponent 1
-    while expQueue:
-        expList = expQueue.popleft() # Gets first item in the list
-        maxExp = max(expList)
-        foundExp = []
-        # Finds all combinations of exponents from the current list
-        for ind, oldExp1 in enumerate(expList):
-            for oldExp2 in expList[ind:]:
-                newExp = oldExp1 + oldExp2
 
-                if newExp < maxExp:
-                    continue
+res = [-1]*200
+res[0] = 0
 
-                if newExp in foundExp:
-                    continue
+expQueue = deque([(1,)]) # Starts with exponent 1
+while -1 in res:
+    expList = expQueue.popleft() # Gets first item in the list
+    maxExp = max(expList)
+    foundExp = []
+    # Finds all combinations of exponents from the current list
+    for ind, oldExp1 in enumerate(expList):
+        for oldExp2 in expList[ind:]:
+            newExp = oldExp1 + oldExp2
 
-                if newExp == exp: # Found match
-                    return len(expList)
+            if newExp > 200:
+                continue
 
-                if newExp not in expList: # Found new exponent
-                    foundExp.append(newExp)
-                    expQueue.append( (newExp,)+expList )
+            if newExp < maxExp:
+                continue
 
-print sum(map(effExp,range(1,201)))
+            if newExp in foundExp:
+                continue
+
+            if res[newExp-1] == -1: # Found match
+                res[newExp-1] = len(expList)
+
+            if newExp not in expList: # Found new exponent
+                foundExp.append(newExp)
+                expQueue.append( (newExp,)+expList )
+
+print sum(res)
