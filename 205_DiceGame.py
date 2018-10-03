66e69395a3f7beb434f5ad1462814b6eda600d2b
Ehsan Kia <contact@ehsankia.com>
2018-10-03 00:52:44 -0700

    Add comments to solution for 205

--- 205_DiceGame.py
+++ 205_DiceGame.py
@@ -1,6 +1,7 @@
 import collections
 import itertools
 
+# Calculate number of ways to get each total
 peter = collections.defaultdict(int)
 for roll in itertools.product(range(1, 5), repeat=9):
     peter[sum(roll)] += 1
@@ -9,16 +10,21 @@ colin = collections.defaultdict(int)
 for roll in itertools.product(range(1, 7), repeat=6):
     colin[sum(roll)] += 1
 
+# calculate total ways to roll dies
 peter_total = sum(peter.values())
 colin_total = sum(colin.values())
 
+# Turn into probability array
 peter_arr = [peter[i] / float(peter_total) for i in range(37)]
 colin_arr = [colin[i] / float(colin_total) for i in range(37)]
 
+# Calculate cumulative probability array
 peter_cum = [sum(peter_arr[i:]) for i in range(37)]
 
+# Calculate probability of peter winning for each possible value
 probability = 0
 for i in range(36):
     probability += colin_arr[i] * peter_cum[i + 1]
 
+# Print to 7 digits
 print '%.7f' % probability
