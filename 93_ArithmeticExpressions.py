89ccb9099575b2e630251b0b8bf374ec6b8896fa
Ehsan Kia <contact@ehsankia.com>
2018-10-05 13:25:49 -0700

    Clean up #93 even more

--- 93_ArithmeticExpressions.py
+++ 93_ArithmeticExpressions.py
@@ -16,22 +16,17 @@ def arithmetic_outputs_run_length(digits):
     Returns:
         Longest consecutive run length
     """
-    assert len(digits) == 4, "There must be exactly 4 digits in the list"
-    assert [0 <= d < 10 for d in digits], "All 4 elements must be single digit"
-    assert [isinstance(d, int) for d in digits], "All elements must be integers"
-
     outputs = set()
     for a, b, c, d in itertools.permutations(digits):
         for op1, op2, op3 in itertools.product(OPS, repeat=3):
-            result1 = op1(op2(op3(a, b), c), d)
-            if result1 > 0 and float(result1).is_integer():
-                outputs.add(result1)
-            result2 = op1(op2(a, b), op3(c, d))
-            if result2 > 0 and float(result2).is_integer():
-                outputs.add(result2)
+            outputs.update([
+                op1(op2(op3(a, b), c), d),  # (((a . b) . c) . d)
+                op1(op2(a, b), op3(c, d)),  # ((a . b) . (c . d))
+            ])
 
     # Computes the length of the consecutive run from 1 to n in the outputs.
     return next(i for i in itertools.count(1) if i not in outputs) - 1
 
 
+# Find the 4 digits that produce the longest run length
 print max(itertools.combinations(range(1, 10), 4), key=arithmetic_outputs_run_length)
